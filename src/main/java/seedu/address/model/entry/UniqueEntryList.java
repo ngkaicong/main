83e28d9759b8e8b6adb93b16fc85949967057143
Pang Yu Shao <p.yushao2@gmail.com>
2019-03-18 02:01:26 +0800

    Made adding duplicate entries possible

--- src/main/java/seedu/address/model/entry/UniqueEntryList.java
+++ src/main/java/seedu/address/model/entry/UniqueEntryList.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import seedu.address.model.entry.exceptions.DuplicateEntryException;
 import seedu.address.model.entry.exceptions.EntryNotFoundException;
 
 /**
