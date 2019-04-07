6ada0ef5837489a285e23126901d363fa6c00961
yushao2 <p.yushao2@gmail.com>
2019-04-08 01:45:01 +0800

    Improved performance for the crypto commands -- reduced lagging

--- src/main/java/seedu/address/logic/commands/EthereumCommand.java
+++ src/main/java/seedu/address/logic/commands/EthereumCommand.java
@@ -9,6 +9,7 @@ import java.net.URL;
 import java.util.function.Predicate;
 
 import javafx.collections.ObservableList;
+import seedu.address.commons.util.CryptoUtil;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.entry.CashFlow;
@@ -36,49 +37,22 @@ public class EthereumCommand extends Command {
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
         double price = 0.0;
-        try {
-            URL url = new URL("https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH&tsyms=USD");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
+        CryptoUtil cryptoUtil = CryptoUtil.getInstance();
+        price = cryptoUtil.getETH();
 
-            if (conn.getResponseCode() != 200) {
-                throw new RuntimeException("Failed : HTTP error code : "
-                        + conn.getResponseCode());
-            }
-
-            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-
-//            System.out.println("Output from Server .... \n");
-            String output = br.readLine();
-//            System.out.println(output);
-            //            while ((output = br.readLine()) != null) {
-            //                System.out.println(output);
-            //                temp = output;
-            //            }
-//            System.out.println(output.substring(14, 21));
-
-            model.updateFilteredEntryList(this.predicate);
-            ObservableList<Entry> filteredList = model.getFilteredEntryList();
-            ReportEntryList reportList = new ReportEntryList(filteredList);
-            Double total = reportList.getTotal();
+        model.updateFilteredEntryList(this.predicate);
+        ObservableList<Entry> filteredList = model.getFilteredEntryList();
+        ReportEntryList reportList = new ReportEntryList(filteredList);
+        Double total = reportList.getTotal();
 //            System.out.println(total);
 
-            price = Float.parseFloat(output.substring(14, 18));
 //            System.out.println(price);
 
-            Double amount = total / price;
-            amount = (double) Math.round(amount * 100.0) / 100.0;
+        Double amount = total / price;
+        amount = (double) Math.round(amount * 100.0) / 100.0;
 
-            MESSAGE_SUCCESS = MESSAGE_SUCCESS + amount.toString() + " ETH.";
 
-            conn.disconnect();
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        MESSAGE_SUCCESS = MESSAGE_SUCCESS + amount.toString() + " ETH.";
 
         int roundOff = (int) Math.round(price);
 
