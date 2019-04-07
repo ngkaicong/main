1d1b7ebf6c7d66d9e5f5c019fdd15997fda84bfe
dom <frankquekch@gmail.com>
2019-04-07 23:22:24 +0800

    ug update

--- src/main/java/seedu/address/ui/ReportWindow.java
+++ src/main/java/seedu/address/ui/ReportWindow.java
@@ -14,7 +14,7 @@ import javafx.scene.text.Text;
 import javafx.scene.web.WebView;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-import org.jetbrains.annotations.NotNull;
+//import org.jetbrains.annotations.NotNull;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.Logic;
 import seedu.address.logic.parser.ReportCommandParser;
