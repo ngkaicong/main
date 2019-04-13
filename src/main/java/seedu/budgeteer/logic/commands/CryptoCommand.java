b2dd22c08d8e4d64bbe7110628410b991f82307d
jacobhan <jacobhan@Jacobs-MacBook-Pro.local>
2019-04-13 13:27:13 +0800

    added crypto command

---
+++ src/main/java/seedu/budgeteer/logic/commands/CryptoCommand.java
@@ -0,0 +1,116 @@
+package seedu.budgeteer.logic.commands;
+
+import javafx.collections.ObservableList;
+import seedu.budgeteer.logic.CommandHistory;
+import seedu.budgeteer.model.Model;
+import seedu.budgeteer.model.entry.Entry;
+import seedu.budgeteer.model.entry.Name;
+import seedu.budgeteer.model.entry.ReportEntryList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static seedu.budgeteer.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.budgeteer.model.Model.PREDICATE_SHOW_ALL_ENTRYS;
+
+/**
+ * Returns how much cryptocurrency you can buy at the current market price.
+ */
+public class CryptoCommand extends Command {
+
+    public static final String COMMAND_WORD = "crypto";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Displays how much of a certain cryptocurrency you can buy.\n"
+            + "Parameters: "
+            + PREFIX_NAME + "NAME "
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_NAME + "CRYPTOCURRENCY NAME "
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_NAME + "MSFT";
+
+    private static final String MESSAGE_SUCCESS = "The price of the cryptocurrency ";
+
+    private String firstUrl = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=";
+    private String crypto = "MSFT";
+    private String secondUrl = "&apikey=Y6G36I3BIPQL5I2";
+
+    private final Name name;
+
+    public CryptoCommand(Name name) {
+        this.name = name;
+    }
+
+    /**
+     * Function that calls the crypto API and returns the JSON in string format
+     */
+    public String cryptoPrice() {
+        String ret = "";
+        try {
+            String temp = firstUrl + name.fullName + secondUrl;
+
+            URL url = new URL(temp);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+            String output;
+            int lineNumber = 7;
+            while (lineNumber > 0) {
+                output = br.readLine();
+                ret = output;
+                lineNumber -= 1;
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) {
+        double price;
+        String messageReturn;
+
+        model.updateFilteredEntryList(PREDICATE_SHOW_ALL_ENTRYS);
+        ObservableList<Entry> filteredList = model.getFilteredEntryList();
+        ReportEntryList reportList = new ReportEntryList(filteredList);
+        Double total = reportList.getTotal();
+
+        String full = cryptoPrice();
+
+        if (full == null) {
+            messageReturn = "Sorry, your input is not a valid cryptocurrency. Please try again.";
+        } else if (full.length() < 30) {
+            messageReturn = "Sorry, your input is not a valid cryptocurrency. Please try again.";
+        } else {
+            price = Float.parseFloat(full.substring(22, 28));
+            Double printPrice = (double) Math.round(price * 100.0) / 100.0;
+
+            String first = "You are able to buy ";
+            Double amount = total / price;
+            amount = (double) Math.round(amount * 100.0) / 100.0;
+            String second = first + amount + " " + name.fullName + " cryptocurrency. ";
+
+            messageReturn = second + "The price of the cryptocurrency " + name.fullName
+                    + " is $" + printPrice.toString() + ".";
+        }
+
+        return new CommandResult(messageReturn);
+    }
+}
---
+++ src/main/java/seedu/budgeteer/logic/parser/CryptoCommandParser.java
@@ -0,0 +1,43 @@
+package seedu.budgeteer.logic.parser;
+
+import seedu.budgeteer.logic.commands.StockCommand;
+import seedu.budgeteer.logic.parser.exceptions.ParseException;
+import seedu.budgeteer.model.entry.Name;
+
+import java.util.stream.Stream;
+
+import static seedu.budgeteer.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.budgeteer.logic.parser.CliSyntax.PREFIX_NAME;
+
+/**
+ * Parses input arguments and creates a new FindCommand object
+ */
+public class CryptoCommandParser implements Parser<StockCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FindCommand
+     * and returns an FindCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public StockCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, StockCommand.MESSAGE_USAGE));
+        }
+
+        Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
+
+        return new StockCommand(name);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+
+}
